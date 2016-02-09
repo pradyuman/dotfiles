555837784a6db577426ebcdf0480c2f77539370d
Pradyuman Vig <pradyumanvig@outlook.com>
2016-02-08 22:01:05 -0500

    Updated courselinks

--- powershell/Modules/courselinks/courselinks.psm1
+++ powershell/Modules/courselinks/courselinks.psm1
@@ -8,4 +8,24 @@ function open-301 {
 }
 Set-Alias w301 open-301
 
+function open-362 {
+	Start-Process -FilePath "https://engineering.purdue.edu/ece362/index.html"
+}
+Set-Alias w362 open-362
+
+function open-369 {
+	Start-Process -FilePath "https://engineering.purdue.edu/ee369/"
+}
+Set-Alias w369 open-369
+
+function open-364 {
+	Start-Process -FilePath "https://piazza.com/purdue/spring2016/ece364/home"
+}
+Set-Alias w364 open-364
+
+function open-473 {
+	Start-Process -FilePath "https://engineering.purdue.edu/~ee473/"
+}
+Set-Alias w473 open-473
+
 Export-ModuleMember -Function * -Alias *
