2d92b90e6e13869db4d80c2512efcb9053623c86
Pradyuman Vig <pradyumanvig@outlook.com>
2018-07-08 16:00:02 -0700

    add vscode keybindings and settings

---
+++ vscode/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp $HOME/Library/Application\ Support/Code/User/settings.json ~/dotfiles/vscode 
+cp $HOME/Library/Application\ Support/Code/User/keybindings.json ~/dotfiles/vscode
