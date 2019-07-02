01b5fa2957bd52f2b4a90fd6f4447f5d91bf484a
Pradyuman Vig <me@pradyuman.co>
2019-07-02 13:14:29 -0700

    remove applications from nix config

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -6,43 +6,19 @@ in {
   home.file.".gitconfig".source = "${dotfiles}/.gitconfig";
 
   home.packages = with pkgs; [
-    brave
-    discord
-    slack
-    signal-desktop
-    vscode
-
-    dhall
-    dhall-json
-
+    git
+    gnupg
     jq
     parallel
     protobuf
     yarn
-    nodejs-10_x
     python
-
     awscli
     unzip
   ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  services = {
-    gpg-agent = {
-      enable = true;
-      defaultCacheTtl = 7 * 24 * 3600;
-      maxCacheTtl = 7 * 24 * 3600;
-      enableSshSupport = true;
-    };
-  };
-
-  nixpkgs.config = {
-    packageOverrides = pkgs: {
-      yarn = pkgs.yarn.override { nodejs = pkgs.nodejs-10_x; };
-    };
-  };
 
   nixpkgs.config.allowUnfree = true;
 }
