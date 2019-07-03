e691f417e237f82bde5287d2764a4534ebbd0177
Pradyuman Vig <me@pradyuman.co>
2019-07-03 14:17:32 -0700

    hyper symlink through nix

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
-{
+let
+  dotfiles = "${config.home.homeDirectory}/dotfiles";
+in {
+  home.file.".hyper.js".source = "${dotfiles}/.hyper.js";
+
   imports = [
     ./git.nix
     ./vscode.nix
