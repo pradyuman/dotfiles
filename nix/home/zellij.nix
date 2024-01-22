1f799c0b81633f2f7079760daee42ec2880dec92
Pradyuman Vig <me@pradyuman.co>
2024-01-21 21:36:21 -0600

    zenwritten dark theme for zellij

--- nix/home/zellij.nix
+++ nix/home/zellij.nix
@@ -8,6 +8,23 @@
       default_layout = "compact";
       pane_frames = false;
       simplified_ui = true;
+
+      theme = "zenwritten_dark";
+      themes = {
+        zenwritten_dark = {
+          fg = "#bbbbbb";
+          bg = "#191919";
+          black = "#191919";
+          red = "#de6e7c";
+          green = "#819b69";
+          yellow = "#b77e64";
+          blue = "#6099c0";
+          magenta = "#b279a7";
+          cyan = "#66a5ad";
+          white = "#bbbbbb";
+          orange = "#b77e64";
+        };
+      };
     };
   };
 }
